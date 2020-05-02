f5cb1596d7b18c9c08040e100599ed0cb87145c0
Zacck Osiemo <coderv63@gmail.com>
2020-05-02 07:28:33 +0300

    Implement more items case and test

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,23 @@ macro_rules! avec {
     };
     // outer curly braces is for macro rules
     // inner curly braces is the block the macro expands to
-    ($element:expr) => {{
+    ($($element:expr),+ /* + means one or more exprs separated by a comma*/) => {{
         let mut vs = Vec::new();
-        vs.push($element);
+        // repeat this operation as many times
+        // the same number of times as the pattern that had element in it
+        $(vs.push($element);)*
         vs
     }};
+}
+
+#[test]
+fn double() {
+    let x: Vec<u32> = avec![421, 43];
+    assert!(!x.is_empty());
+    assert_eq!(x.len(), 2);
+    //assert eq is a declarative macro and can use any delimiter
+    assert_eq![x[0], 421];
+    assert_eq![x[1], 43];
 }
 
 #[test]

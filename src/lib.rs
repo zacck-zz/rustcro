f3a25a13fd191b0df6de2ec263089d7782c88ab6
Zacck Osiemo <coderv63@gmail.com>
2020-05-02 07:13:35 +0300

    Implement singleton case'

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,22 @@ macro_rules! avec {
     () => {
         Vec::new()
     };
+    // outer curly braces is for macro rules
+    // inner curly braces is the block the macro expands to
+    ($element:expr) => {{
+        let mut vs = Vec::new();
+        vs.push($element);
+        vs
+    }};
+}
+
+#[test]
+fn single() {
+    let x: Vec<u32> = avec![421];
+    assert!(!x.is_empty());
+    assert_eq!(x.len(), 1);
+    //assert eq is a declarative macro and can use any delimiter
+    assert_eq![x[0], 421];
 }
 
 #[test]
